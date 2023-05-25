f5cfc40e3552dd9cd2cc7326fa025557d9151a13
joshuamwo <joshuamuuo5@gmail.com>
2023-05-25 09:16:09 +0300

    modularised nav components

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+import { styles } from "@/utils/styles";
+import LogoAndName from "./navComponents/LogoAndName";
+import NavLinksComponent from "./navComponents/NavLinksComponent";
+import MobileMenu from "./navComponents/MobileMenu";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
 
-export default Navbar
+  const navClicked = (itemClicked) => {
+    setActive(itemClicked);
+  };
+  const menuToggled = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <nav
+      className={` ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <LogoAndName navClicked={navClicked} />
+        <NavLinksComponent navClicked={navClicked} active={active} />
+        <MobileMenu
+          toggle={toggle}
+          menuToggled={menuToggled}
+          active={active}
+          navClicked={navClicked}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
