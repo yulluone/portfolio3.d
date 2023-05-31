d2bed4117f9c0085b57eabe292df29bbcd77e528
joshuamwo <joshuamuuo5@gmail.com>
2023-05-31 10:25:32 +0300

    Tech section complete

--- components/canvas/Ball.jsx
+++ components/canvas/Ball.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+// Import dependencies from the @react-three/drei package
+import { Decal, Float, useTexture } from "@react-three/drei";
 
-const Ball = () => {
+// Define Ball component
+const Ball = (props) => {
+  // Use the useTexture hook to load the image specified by the imgUrl prop
+  const [decal] = useTexture([props.imgUrl]);
+
+  // Render the 3D ball mesh
   return (
-    <div>Ball</div>
-  )
-}
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        {/* Add a Decal component to apply the loaded texture to the ball */}
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          map={decal}
+        />
+      </mesh>
+    </Float>
+  );
+};
 
-export default Ball
+// Export Ball component
+export default Ball;
---
+++ components/canvas/BallCanvas.jsx
@@ -0,0 +1,29 @@
+// Import React dependencies
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload } from "@react-three/drei";
+
+// Import custom components
+import CanvasLoader from "./Loader";
+import Ball from "./Ball";
+
+// Define BallCanvas component
+const BallCanvas = ({ icon }) => {
+  return (
+    // Create a new Canvas component from the @react-three/fiber package
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+      {/* Use the Suspense component to handle loading states for the Ball component */}
+      <Suspense fallback={<CanvasLoader />}>
+        {/* Add OrbitControls component for mouse and touch controls */}
+        <OrbitControls enableZoom={false} />
+        {/* Render the Ball component and pass the icon prop */}
+        <Ball imgUrl={icon.src} />
+      </Suspense>
+      {/* Use the Preload component to preload assets used in the Canvas */}
+      <Preload all />
+    </Canvas>
+  );
+};
+
+// Export BallCanvas component
+export default BallCanvas;
