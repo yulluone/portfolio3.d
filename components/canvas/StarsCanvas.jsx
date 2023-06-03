cd5402a9e6ed676bafb9e590654411016fc2def9
joshuamwo <joshuamuuo5@gmail.com>
2023-06-03 18:42:04 +0300

    stars done

---
+++ components/canvas/StarsCanvas.jsx
@@ -0,0 +1,21 @@
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
+
+import Stars from "./Stars";
+
+const StarsCanvas = () => {
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarsCanvas;
--- components/canvas/index.js
+++ components/canvas/index.js
@@ -1,6 +1,6 @@
 import EarthCanvas from "./EarthCanvas";
 import BallCanvas from "./BallCanvas";
 import ComputersCanvas from "./ComputersCanvas";
-import StarsCanvas from "./Stars";
+import StarsCanvas from "./StarsCanvas";
 
 export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
